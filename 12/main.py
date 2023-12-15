6aca2b1acdbeb1007b91a6a1ecb9413e053fd98d
Kristaps <kbebris+git@gmail.com>
2023-12-15 17:59:31 +0200

    swapped out day 12 implementation with one that runs at a reasonable pace

--- 12/main.py
+++ 12/main.py
@@ -1,3 +1,4 @@
+from functools import cache
 # https://adventofcode.com/2023/day/12
 
 
@@ -10,37 +11,41 @@ conds = [l.split(" ")[0] for l in lines]
 groups = [[int(c) for c in l.split(" ")[1].split(",")] for l in lines]
 
 
-# TODO: this is horribly slow - I tried to look for a bit of inspiration and I guess the solution is to go character by character
-# and to cache whatever the state at a given character was - e.g. - https://github.com/ypisetsky/advent-of-code-2023/blob/main/day12.py
+# copied speedier implementation from https://github.com/ypisetsky/advent-of-code-2023/blob/main/day12.py
 # note to self - brush up on dynamic programming
-def find_valid_states(curr_s, group, new_group_cap, conds, c_len, min_common, min_space=1):
-    if len([i for i in range(min_common) if curr_s[i] != conds[i] and conds[i] != '?']) == 0:
-        if len(group) == 0:
-            if len([c for c in conds[min_common:] if c == '#']) == 0 and len([c for c in curr_s[min_common:] if c == '#']) == 0:
-                return 1
+# TODO: take a look at https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F2023-day-12-part-2-this-image-helped-a-few-people-to-solve-v0-46y3l042m06c1.png%3Fs%3Dd944fffd4f3b4f73ac26c336af377d35bd364e9f
+# looks like a cool way to go about it
+@cache
+def count_valid_states(line, counts, pos, current_count, countpos):
+      # pos is the next character to be processed
+      # current_count is how far into the current sequence of #s we are in
+      # countpos is how many sequences of #s we have already finished
+      if pos == len(line):
+            ret = 1 if len(counts) == countpos else 0
+      elif line[pos] == '#':
+            ret = count_valid_states(line, counts, pos + 1, current_count + 1, countpos)
+      elif line[pos] == '.' or countpos == len(counts):
+            if countpos < len(counts) and current_count == counts[countpos]:
+                  ret = count_valid_states(line, counts, pos + 1, 0, countpos + 1)
+            elif current_count == 0:
+                  ret = count_valid_states(line, counts, pos + 1, 0, countpos)
             else:
-                return 0
-        else:
-            min_l = len(f".{group[0]}")
-            if min_l > c_len:
-                return 0
-            else:
-                rets = []
-                for i in range(c_len-min_l-new_group_cap):
-                    new_s = f'{"."*(i+min_space)}{"#"*group[0]}'
-                    new_group_cap = sum(group[2:])+len(group)-3
-                    new_conds = conds[len(curr_s):]
-                    new_c_len = len(new_conds)
-                    rets.append(find_valid_states(new_s, group[1:], new_group_cap, new_conds, new_c_len, min(len(new_s), new_c_len)))
-                return sum(rets)
-    else:
-        return 0
+                  ret = 0
+      else:
+            hash_count = count_valid_states(line, counts, pos + 1, current_count + 1, countpos)
+            dot_count = 0
+            if current_count == counts[countpos]:
+                  dot_count = count_valid_states(line, counts, pos + 1, 0, countpos + 1)
+            elif current_count == 0:
+                  dot_count = count_valid_states(line, counts, pos + 1, 0, countpos)
+            ret = hash_count + dot_count
+      return ret
 
 
 # PART ONE
 res = []
 for i in range(len(conds)):
-    res.append(find_valid_states("", groups[i], sum(groups[i][1:])+len(groups[i])-3, conds[i], len(conds[i]), 0, 0))
+    res.append(count_valid_states(conds[i] + '.', tuple(groups[i]), 0, 0, 0))
 
 #print(res)
 print(f"PART ONE: {sum(res)}")
@@ -51,7 +56,7 @@ res = []
 for i in range(len(conds)):
     s_group = groups[i]*scale_f
     s_conds = "?".join([conds[i] for _ in range(scale_f)])
-    res.append(find_valid_states("", s_group, sum(s_group[1:])+len(s_group)-3, s_conds, len(s_conds), 0, 0))
+    res.append(count_valid_states(s_conds+'.', tuple(s_group), 0, 0, 0))
 
 #print(res)
 print(f"PART TWO: {sum(res)}")
