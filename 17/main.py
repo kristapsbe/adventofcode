1a53ada07a96112ddd5f0b67ed2fbbc548cab201
Kristaps <kbebris+git@gmail.com>
2023-12-17 12:33:34 +0200

    non working brute force day 17 python

--- 17/main.py
+++ 17/main.py
@@ -13,15 +13,40 @@ w = len(lines[0])
 min_p = h*w*999
 max_s = 3 # max number of cells we're allowed to move straight
 # starting to the right and to the bottom 
-# (current i, current j, direction i, direction j, how moving straight, turn counter, current heat)
-ns = [(0, 0, 0, 1, 0, 0, 0), (0, 0, 1, 0, 0, 0, 0)] 
+# (current i, current j, how moving straight, turn counter, current heat)
+ns = [(0, 0, 0, 1, 0), (0, 0, 0, 2, 0)] 
+moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+move_l = len(moves)-1
 
 while len(ns) > 0: # keep going while we have paths to explore
+    # try to enter nodes we had planned to enter
     tmp_ns = []
-    # first - try to enter nodes we had planned to enter
     for n in ns:
-        ni = n[0]+n[2]
-        nj = n[1]+n[3]
-        nstr = n[4]+1
-        
-    ns = tmp_ns
+        ni = n[0]+moves[n[3]][0]
+        nj = n[1]+moves[n[3]][1]
+        #print(f"{ni} {nj} {h} {w}")
+        if ni >= 0 and nj >= 0 and ni < h and nj < w: 
+            #print(f"{ni} {nj}")
+            np = n[4]+lines[ni][nj]
+            if np < min_p:
+                if ni == (h-1) and nj == (w-1):
+                    min_p = np
+                else:
+                    # note - even if we turned to get here we're always technically making a move straight ahead
+                    tmp_ns.append((ni, nj, n[2]+1, n[3], np))
+    # figure out what potential directions could we go from the nodes that we've entered
+    next_ns = []
+    for n in tmp_ns:
+        if n[2] < max_s: # not exceeded straight move allowance - let it keep going
+            next_ns.append(n)
+        if n[3] > 1: # no loop to the right - add route to the right
+            next_ns.append((n[0], n[1], 0, n[3]-1, n[4]))
+        if n[3] < move_l: # no loop to the left - add route to the left
+            next_ns.append((n[0], n[1], 0, n[3]+1, n[4]))
+    #print()
+    #print(ns)
+    #print(tmp_ns)
+    #print(next_ns)
+    ns = next_ns
+
+print(min_p)
