d00801f5f5cb586737dadc6cd5d12c0084577891
Kristaps <kbebris+git@gmail.com>
2023-12-17 17:06:59 +0200

    day 17 python still bugged

--- 17/main.py
+++ 17/main.py
@@ -10,30 +10,28 @@ h = len(lines)
 w = len(lines[0])
 
 # PART ONE
-min_p = h*w*999
+min_p = 999999
 max_s = 3 # max number of cells we're allowed to move straight
 # starting to the right and to the bottom 
-# (current i, current j, how moving straight, turn counter, current heat)
+# (current i, current j, how long moving straight, direction id, current heat)
 ns = [(0, 0, 0, 1, 0), (0, 0, 0, 2, 0)] 
 moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-move_l = len(moves)-1
+visited = [[{} for _ in range(w)] for _ in range(h)]
 
 while len(ns) > 0: # keep going while we have paths to explore
-    # try to enter nodes we had planned to enter
     tmp_ns = []
-    for n in ns:
+    for n in ns: # figure out which cells we can move into
         ni = n[0]+moves[n[3]][0]
         nj = n[1]+moves[n[3]][1]
-        #print(f"{ni} {nj} {h} {w}")
-        if ni >= 0 and nj >= 0 and ni < h and nj < w: 
-            #print(f"{ni} {nj}")
-            np = n[4]+lines[ni][nj]
-            if np < min_p:
-                if ni == (h-1) and nj == (w-1):
-                    min_p = np
-                else:
+        if ni >= 0 and nj >= 0 and ni < h and nj < w: # check if we're not trying to fall off the board
+            np = n[4]+lines[ni][nj] # the heat that we're getting
+            p_id = (n[2], n[3])
+            if p_id not in visited[ni][nj] or np < visited[ni][nj][p_id]:
+                visited[ni][nj][p_id] = np
+                if ni != (h-1) or nj != (w-1):
                     # note - even if we turned to get here we're always technically making a move straight ahead
                     tmp_ns.append((ni, nj, n[2]+1, n[3], np))
+
     # figure out what potential directions could we go from the nodes that we've entered
     next_ns = []
     for n in tmp_ns:
@@ -41,12 +39,9 @@ while len(ns) > 0: # keep going while we have paths to explore
             next_ns.append(n)
         if n[3] > 1: # no loop to the right - add route to the right
             next_ns.append((n[0], n[1], 0, n[3]-1, n[4]))
-        if n[3] < move_l: # no loop to the left - add route to the left
+        if n[3] < len(moves)-1: # no loop to the left - add route to the left
             next_ns.append((n[0], n[1], 0, n[3]+1, n[4]))
-    #print()
-    #print(ns)
-    #print(tmp_ns)
-    #print(next_ns)
     ns = next_ns
 
-print(min_p)
+for v in visited:
+    print(v)
