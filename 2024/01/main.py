b55457660dc682324aafea09c74dbabbff6c7292
Kristaps <kbebris+git@gmail.com>
2024-12-01 12:33:57 +0200

    don't need to specify 3 whitespaces

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,6 +1,6 @@
 lists = []
 with open("input.txt", "r") as f:
-    lists = [[int(i) for i in l.strip().split("   ")] for l in f.readlines()]
+    lists = [[int(i) for i in l.strip().split()] for l in f.readlines()]
 first, second = list(zip(*lists))
 
 print(f"PART ONE: {sum([abs(f-s) for f, s in zip(sorted(first), sorted(second))])}")
