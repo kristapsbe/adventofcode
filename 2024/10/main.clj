5d486f38026f282d0750da001f15fcb0533d6f31
Kristaps <kbebris+git@gmail.com>
2024-12-10 18:43:38 +0200

    cloj

--- 2024/10/main.clj
+++ 2024/10/main.clj
@@ -5,9 +5,15 @@
 
 (def data (mapv #(clojure.string/split % #"") (clojure.string/split-lines (slurp "test.txt"))))
 (def idata (cast-values Integer/parseInt data))
+(def h (count idata))
+(def w (count (first idata)))
 
-(defn find-trails [coll i j c r do-check]
-    (let [nc (inc c)] (println nc)))
+(defn in? [coll elm]
+    (some #(= elm %) coll))
 
-(println (find-trails idata 0 0 0 [] true))
-(println (vec (map-indexed (fn [i x] (println i x)) idata)))
+(defn find-trails [i j c r do-check]
+    (let [nc (inc c)]
+        (println nc)))
+
+(println (find-trails 0 0 0 [] true))
+(println (vec (map-indexed (fn [i x] (vec (map-indexed (fn [j y] (println i j y)) x))) idata)))
