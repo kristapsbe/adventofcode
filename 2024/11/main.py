118bb848f1f97bb670ee13732a32f155ae8a1ec5
Kristaps <kbebris+git@gmail.com>
2024-12-11 13:31:09 +0200

    day 11 python

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -2,23 +2,29 @@ input = [int(c) for c in open("input.txt", "r").read().strip().split()]
 
 print(input)
 
-def brute_force_ftw(input, ct):
-    for c in range(ct):
-        tmp = []
-        for i in input:
-            if i == 0:
-                tmp.append(1)
-            elif len(str(i))%2 == 0:
-                si = str(i)
-                sl = int(len(si)/2)
-                tmp.append(int(si[:sl]))
-                tmp.append(int(si[sl:]))
-            else:
-                tmp.append(i*2024)
+def memoize(func):
+    cache = {}
+    def inner(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return inner
 
-        input = tmp
-        print(len(input), "|", c, "/", ct)
-    return input
+@memoize
+def calc_len(i, d):
+    i_len_mod = len(str(i)) % 2
+    if d == 1:
+        return 2 - i_len_mod
+    else:
+        if i == 0:
+            return calc_len(1, d-1)
+        elif i_len_mod == 0:
+            si = str(i)
+            sl = int(len(si)/2)
+            return calc_len(int(si[:sl]), d-1) + calc_len(int(si[sl:]), d-1)
+        else:
+            return calc_len(i*2024, d-1)
 
-print(brute_force_ftw(input, 25))
-print(brute_force_ftw(input, 75))
+
+print(sum([calc_len(i, 25) for i in input]))
+print(sum([calc_len(i, 75) for i in input]))
