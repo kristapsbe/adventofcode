65cf7143a170d47b705868fd37cb652642f66d9f
Kristaps <kbebris+git@gmail.com>
2025-12-01 20:45:31 +0200

    minor refactor

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -3,19 +3,16 @@ p1 = 0
 p2 = 0
 
 with open("input.txt", "r") as f:
-    for l in f:
+    for i, l in enumerate(f):
         dir = l[0]
         dist = int(l[1:])
 
-        # TODO: fix so that I don't have to iterate - getting a bit too much
-        # if l[0] == "L":
-        #     pos -= int(l[1:])
-        # else:
-        #     pos += int(l[1:])
-        # p2 += abs(pos // 100)
+        # full circles
+        p2 += dist // 100
+        dist %= 100  # TODO: there's probs a better way, but this, at least, shortens the loop
 
         for _ in range(dist):
-            if l[0] == "L":
+            if dir == "L":
                 pos -= 1
             else:
                 pos += 1
@@ -25,5 +22,6 @@ with open("input.txt", "r") as f:
         pos %= 100
         if pos == 0:
             p1 += 1
+
 print(p1)
 print(p2)
